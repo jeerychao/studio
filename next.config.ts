20fb5897b48393ef85386c6f2cfdd1a45f9f5638
Jie lee <leejie2017@gmail.com>
2025-05-30 08:47:56 +0000

    npm run build报错

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone', // Add this line for standalone output
+  // output: 'standalone', // Add this line for standalone output
   typescript: {
     // ignoreBuildErrors: true, // Temporarily remove to surface potential errors
   },
