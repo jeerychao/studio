40e74f175c8a9133303e905ae10bf7679dedf6ce
Jie lee <leejie2017@gmail.com>
2025-06-06 05:07:02 +0000

    请问你修改了吗？针对以上

--- next.config.js
+++ next.config.js
@@ -4,11 +4,8 @@ const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
-      allowedForwardedHosts: ['17.100.100.253:3010'], // Specific forwarded host
-      allowedOrigins: [
-        'http://17.100.100.253:8081', // Actual browser origin
-        'http://17.100.100.253:3010'  // Next.js perceived self-origin
-      ]
+      allowedForwardedHosts: ['17.100.100.253:8081'],
+      allowedOrigins: ['http://17.100.100.253:8081']
     }
   },
   // 禁用客户端缓存以解决 CacheStore 错误 (根据您之前的请求保留)
@@ -18,7 +15,7 @@ const nextConfig = {
       {
         source: '/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'http://17.100.100.253:8081' },
           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'X-Forwarded-Host, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
           // 禁用客户端缓存
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -115,6 +115,7 @@ export default function LoginPage() {
                   onChange={(e) => setEmail(e.target.value)}
                   required
                   disabled={isSubmitting}
+                  autoComplete="email"
                 />
               </div>
               <div className="space-y-2">
@@ -127,6 +128,7 @@ export default function LoginPage() {
                   onChange={(e) => setPassword(e.target.value)}
                   required
                   disabled={isSubmitting}
+                  autoComplete="current-password"
                 />
               </div>
             </CardContent>
