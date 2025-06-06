a733615003d449c365a740bd88a547547e3d03e7
Jie lee <leejie2017@gmail.com>
2025-06-06 10:22:49 +0000

    更新文档

--- src/lib/actions.tsx
+++
@@ -1,4 +0,0 @@
-// This file is intentionally left blank.
-// All server action logic should be in actions.ts.
-// This .tsx file was likely causing build issues or confusion.
-// Keeping it blank and eventually deleting it is recommended if not used for actual JSX components.
